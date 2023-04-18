1e1c75b13770e1a57cc5fd8640394d6c0ba3a372
Ken Javier <kencarlo21@gmail.com>
2023-04-18 17:13:39 +0800

    fixed bug in clear button

--- Disk Scheduling Algorithms.vb
+++ Disk Scheduling Algorithms.vb
@@ -20,12 +20,30 @@
 	RdBtnFCFS.Checked = True
   End Sub
 
-  Private Sub BtnClearAll_Click(sender As Object, e As EventArgs) Handles BtnClearAll.Click
-	For Each ctrl As Control In Me.Controls
-	  If TypeOf ctrl Is TextBox Then
-		CType(ctrl, TextBox).Text = ""
-	  End If
-	Next
-  End Sub
+	Private Sub BtnClearAll_Click(sender As Object, e As EventArgs) Handles BtnClearAll.Click
+		Me.TbAtProcessA.Text = ""
+		Me.TbAtProcessB.Text = ""
+		Me.TbAtProcessC.Text = ""
+		Me.TbAtProcessD.Text = ""
+		Me.TbAtProcessE.Text = ""
 
+		Me.TbCbProcessA.Text = ""
+		Me.TbCbProcessB.Text = ""
+		Me.TbCbProcessC.Text = ""
+		Me.TbCbProcessD.Text = ""
+		Me.TbCbProcessE.Text = ""
+
+		Me.DataGridView1.Columns.Clear()
+		Me.DataGridView1.Rows.Clear()
+
+		Me.RichTextBox2.Text = ""
+	End Sub
+
+	Private Sub TbAtProcessA_TextChanged(sender As Object, e As EventArgs) Handles TbAtProcessA.TextChanged
+
+	End Sub
+
+	Private Sub GroupBox4_Enter(sender As Object, e As EventArgs) Handles GroupBox4.Enter
+
+	End Sub
 End Class
